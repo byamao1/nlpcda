9f353838d8b00738390401a339da93115af0ba48
baiyu <305230444@qq.com>
2020-11-17 19:16:41 +0800

    Change ner_test.py: save as BIO

--- test/ner_test.py
+++ test/ner_test.py
@@ -10,4 +10,10 @@ ner = Ner(ner_dir_name='../write',
           data_augument_tag_list=['mobile'],
           augument_size=10, seed=0, delimiter=' ')
 data_sentence_arrs, data_label_arrs = ner.augment('../write/mobile.txt')
-print(data_sentence_arrs, data_label_arrs)
+# print(data_sentence_arrs, data_label_arrs)
+
+with open('../write/augment_supplement_train.txt', 'w', encoding='utf-8') as f:
+    for list_i in range(len(data_sentence_arrs)):
+        for sentence, label in zip(data_sentence_arrs[list_i], data_label_arrs[list_i]):
+            f.write(str(sentence) + " " + str(label) + "\n")
+        f.write("\n\n")
